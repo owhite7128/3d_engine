cc04f6dddd3fd0098ff88fdbc5728e6b5063d49a
owhite7128 <owhite7128@gmail.com>
2024-04-26 13:55:38 -0600

    updated renderer, using z-cull, issues with normals fundamental flaw

--- include/render.c
+++ include/render.c
@@ -126,14 +126,6 @@ void check_occlusion (MODEL* check_mod, float* z_buff) {
     return;
 }
 
-/*void fill_zbuff (MODEL_LIST* modlist, float* z_buff) {
-    MODEL_NODE* curr = modlist->head;
-    while (curr)
-    {
-        check_occlusion (*(curr->curr), z_buff);
-        curr = curr->next;
-    }
-}*/
 
 float sign (VEC2D pt, VEC2D v1, VEC2D v2)
 {
@@ -165,54 +157,6 @@ float calcZ(VEC3D p1, VEC3D p2, VEC3D p3, float x, float y) {
     return l1 * p1.k + l2 * p2.k + l3 * p3.k;
 }
 
-
-/*
-Interpolate (i0, d0, i1, d1) {
-    values = []
-    a = (d1 - d0) / (i1 - i0)
-    d = d0
-    for i = i0 to i1 {
-        values.append(d)
-        d = d + a
-    }
-    return values
-}
-*/
-int* interpolate (int i0, int d0, int i1, int d1)
-{
-    int* ret = (int*) malloc((i1 - i0 + 1)*sizeof(int));
-    int a = (d1 - d0) / (i1 - i0);
-    int d = d0;
-
-    ret[0] = i1-i0;
-    for (int i=1; i<(i1-i0 + 1); i++)
-    {
-        ret[i] = d;
-        d += a;
-    }
-    return ret;
-}
-
-int* combine (int* l1, int* l2)
-{
-    int* ret = (int*) malloc ((l2[0] + l1[0])*sizeof(int));
-    ret[0] = l2[0] + l1[0] - 1;
-
-    // x: [xlen, x1, x2, x3, x4]
-    // y: [ylen, y1, y2, y3, y4]
-    // [xlen + ylen -1, x1, x2, x3, y1, y2, y3, y4]
-
-    for (int i=1; i < l1[0]; i++)
-    {
-        ret[i] = l1[i];
-    }
-    for (int i=l1[0]; i<(l2[0] + l1[0]); i++)
-    {
-        ret[i] = l2[i-l1[0]];
-    }
-    return ret;
-}
-
 void swap_vec2d (VEC2D *a, VEC2D *b)
 {
     VEC2D temp;
@@ -228,6 +172,7 @@ void swap_vec2d (VEC2D *a, VEC2D *b)
     b->j = temp.j;
     b->k = temp.k;
 }
+
 
 void render_tri (uint32_t *cbuff, float *zbuff, MODEL* mod, TRIANG tri) {
     VEC2D v1 = mod->tvecs[tri.s];
@@ -293,7 +238,7 @@ void render_tri (uint32_t *cbuff, float *zbuff, MODEL* mod, TRIANG tri) {
     {
         min_y = v3.j + half_height;
     }
-
+    
     int p1 = tri.s;
     int p2 = tri.m;
     int p3 = tri.e;
@@ -303,19 +248,17 @@ void render_tri (uint32_t *cbuff, float *zbuff, MODEL* mod, TRIANG tri) {
     VEC3D B = {mod->rot_vecs[p3].i - mod->rot_vecs[p1].i, mod->rot_vecs[p3].j - mod->rot_vecs[p1].j, mod->rot_vecs[p3].k - mod->rot_vecs[p1].k, 1};
     VEC3D C = {mod->rot_vecs[p2].i - 0 + mod->pos.i, mod->rot_vecs[p2].j - 0 + mod->pos.j, mod->rot_vecs[p2].k - 0 + mod->pos.k, 1};
 
-    VEC3D CROSS = vec_cross(B, A);
+    VEC3D CROSS = vec_cross(A, B);
     VEC3D zero_vec = {0, 0, 0 , 1};
     VEC3D NORM = vec3_norm(CROSS, vec3_len(zero_vec, CROSS));
-    
-
+   
     if (vec3_dot(NORM, C) < 0)
     {
         NORM = vec3_sub(zero_vec, NORM);
+
     }
 
     NORM.w = NORM.i*mod->rot_vecs[p1].i + NORM.j*mod->rot_vecs[p1].j + NORM.k*mod->rot_vecs[p1].k;
-
-    // float avg_z = (mod->rot_vecs[tri.s].k + mod->rot_vecs[tri.m].k + mod->rot_vecs[tri.e].k) / 3;
 
     for (int i=min_y; i<max_y; i++)
     {
@@ -327,111 +270,17 @@ void render_tri (uint32_t *cbuff, float *zbuff, MODEL* mod, TRIANG tri) {
                 //float zval = calcZ(mod->vecs[p1], mod->vecs[p2], mod->vecs[p3], pt_vec.i, pt_vec.j);
                 float tval = NORM.w/(NORM.i*(j-half_width) + NORM.j*(i-half_height) + NORM.k*(state.cam.drwpln.k));
                 float zval = (state.cam.drwpln.k)*tval;
-                if (zbuff[i*SCREEN_WIDTH+j] < zval)
-                {
-                    cbuff[i*SCREEN_WIDTH + j] = tri.color;
-                    zbuff[i*SCREEN_WIDTH + j] = zval;
-                }
+                if (i >= 0 && i < SCREEN_HEIGHT && j >= 0 && j < SCREEN_WIDTH){
+                    if (zbuff[i*SCREEN_WIDTH+j] < zval)
+                    {
+                        cbuff[i*SCREEN_WIDTH + j] = tri.color;
+                        zbuff[i*SCREEN_WIDTH + j] = zval;
+                    }
+                } 
             }
         }
 
     }
-
-
-    /*
-    DrawFilledTriangle (P0, P1, P2, color) {
-    // Sort the points so that y0 <= y1 <= y2
-        if y1 < y0 { swap(P1, P0) }
-        if y2 < y0 { swap(P2, P0) }
-        if y2 < y1 { swap(P2, P1) }
-
-    // Compute the x coordinates of the triangle edges
-        x01 = Interpolate(y0, x0, y1, x1)
-        x12 = Interpolate(y1, x1, y2, x2)
-        x02 = Interpolate(y0, x0, y2, x2)
-
-    // Concatenate the short sides
-        remove_last(x01)
-        x012 = x01 + x12
-
-    // Determine which is left and which is right
-        m = floor(x012.length / 2)
-        if x02[m] < x012[m] {
-            x_left = x02
-            x_right = x012
-        } else {
-            x_left = x012
-            x_right = x02
-        }
-
-    // Draw the horizontal segments
-        for y = y0 to y2 {
-            for x = x_left[y - y0] to x_right[y - y0] {
-                canvas.PutPixel(x, y, color)
-            }
-        }
-    }
-    */
-/*
-   if (mod->tvecs[p1].j > mod->tvecs[p2].j)
-   {
-        temp = p1;
-        p1 = p2;
-        p2 = p1;
-   }
-   if (mod->tvecs[p1].j > mod->tvecs[p3].j)
-   {
-        temp = p1;
-        p1 = p3;
-        p3 = p1;
-   }
-   if (mod->tvecs[p2].j > mod->tvecs[p3].j)
-   {
-        temp = p2;
-        p2 = p3;
-        p3 = p2;
-   }
-
-    int* x01 = interpolate(mod->tvecs[p1].j, mod->tvecs[p1].i, mod->tvecs[p2].j, mod->tvecs[p2].i);
-    int* x12 = interpolate(mod->tvecs[p2].j, mod->tvecs[p2].i, mod->tvecs[p3].j, mod->tvecs[p3].i);
-    int* x02 = interpolate(mod->tvecs[p1].j, mod->tvecs[p1].i, mod->tvecs[p3].j, mod->tvecs[p3].i);
-
-    int* x012 = combine(x01, x12);
-    
-    int* x_left;
-    int* x_right;
-
-    int m = x012[0] / 2;
-    if (x02[m] < x012[m])
-    {
-        x_left = x02;
-        x_right = x012;
-    } else {
-        x_left = x012;
-        x_right = x02;
-    }
-
-    for (int i = min_y; i < max_y; i++)
-    {
-        printf ("{%d, %d}\n", x_left[i - min_y] + half_width, x_right[i - min_y] + half_width);
-        for (int j = x_left[i - min_y]; j < x_right[i - min_y]; j++)
-        {
-            printf ("{%d, %d}\n", j, i);
-        }
-        for (int j = x_left[i - min_y]; j < x_right[i - min_y]; j++)
-        {
-            float zval = (NORM.w - NORM.i*(j - half_width) - NORM.j*(i - half_height))/NORM.k;
-            if (zbuff[i*SCREEN_WIDTH+j] < zval)
-            {
-                cbuff[i*SCREEN_WIDTH + j] = tri.color;
-                zbuff[i*SCREEN_WIDTH + j] = zval;
-            }
-        }
-    }
-    free (x01);
-    free (x02);
-    free (x12);
-    free (x012);*/
 }
 
 void draw_3d (uint32_t *cbuff, float *zbuff, MODEL *mod, CAMERA cam) {
@@ -533,7 +382,6 @@ void render(int render_mode) {
                     case SDL_QUIT:
                         state.quit = 1;
                         break;
-                    
                 }
             }
             // Calls the onRender FXN given by FXN Ptr
--- src/main.c
+++ src/main.c
@@ -10,26 +10,26 @@
 #include <time.h>
 #include <math.h>
 
-#define PI 3.14159265358765
+#define PI 3.14159265358
 
 float t = 0.0;
 int y = -1;
 
 void on_init_rend (WINDOW_APP* app) {
     /*VEC3D mod_vec = {0, 0, -7, 1};
-    ANG3D mod_ang = {0, 0, 0, 1};
+    ANG3D mod_ang = {PI/2, 0, 0, 0};
     add_model (app->mod_list, "./spheretris.obj", mod_vec, mod_ang, 1);*/
 
-    VEC3D mod_vec_2 = {0, 0, -7, 1};
-    ANG3D mod_ang_2 = {0, 0, 0, 1};
+    VEC3D mod_vec_2 = {0, 0, -20, 1};
+    ANG3D mod_ang_2 = {0, PI, 0, 0.1};
     add_model (app->mod_list, "./monkey.obj", mod_vec_2, mod_ang_2, 1);
 
     /*VEC3D mod_vec_3 = {-2, 0, -7, 1};
-    ANG3D mod_ang_3 = {0, 0, 0, 1};
-    add_model (app->mod_list, "./monkey.obj", mod_vec_3, mod_ang_3, 1);
+    ANG3D mod_ang_3 = {PI/2, 0, 0, 0.1};
+    add_model (app->mod_list, "./monkey.obj", mod_vec_3, mod_ang_3, 1);*/
 
-    VEC3D mod_vec_4 = {2, 0, -7, 1};
-    ANG3D mod_ang_4 = {0, 0, 0, 1};
+    /*VEC3D mod_vec_4 = {2, 0, -7, 1};
+    ANG3D mod_ang_4 = {PI/2, 0, 0, 0};
     add_model (app->mod_list, "./monkey.obj", mod_vec_4, mod_ang_4, 1);*/
     
     return;
@@ -37,9 +37,10 @@ void on_init_rend (WINDOW_APP* app) {
 
 // ANG3D mod_rot = {0, PI/2, 0, 0.01};
 void call_rend (WINDOW_APP* app) {
-    
-    ANG3D mod_rot = {PI/2, PI/2, PI/2, t};
+    ANG3D mod_rot = {0, PI, 0, t};
+    VEC3D mod_mov = {0, 0, -7-5-5*sin(t), 1};
     set_model_rot (app->mod_list->head->curr, mod_rot);
+    set_model_pos (app->mod_list->head->curr, mod_mov);
     /*set_model_rot (app->mod_list->head->next->curr, mod_rot);
     set_model_rot (app->mod_list->head->next->next->curr, mod_rot);*/
     //printf ("Z Rot: %f\n", app->mod_list->head->curr->rot.z);
@@ -49,12 +50,6 @@ void call_rend (WINDOW_APP* app) {
     // ANG3D cam_rot = {0, 0, t, 1};
     // update_camera_ang (&(app->cam), cam_rot);
 
-    /*if (y >= 1)
-    {
-        y = -1;
-    } else {
-        y += 0.01;
-    }*/
     //printf ("T: %f\n", t);
     if (t >= 2*PI)
     {
